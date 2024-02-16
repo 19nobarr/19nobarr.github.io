3e714d4cbba05300d1e6a3003388f4f35c446d84
Noah Barr <barrn@umich.edu>
2024-02-16 16:32:55 -0500

    added the other screens. TODO implement responsiveness for Mobile

--- pages/code.js
+++ pages/code.js
@@ -10,18 +10,17 @@ function clickNav() {
 }
 
 function navToHome() {
-  window.location.href = "./index.html";
+  window.location.href = "../index.html";
 }
 
 function navToAbout() {
-  window.location.href = "/pages/about.html";
+  window.location.href = "./about.html";
 }
 
 function navToContact() {
-  window.location.href = "./pages/contact.html";
+  window.location.href = "./contact.html";
 }
 
 function navToWork() {
-  window.location.href = "./pages/work.html";
-  console.log("fsdf");
+  window.location.href = "./work.html";
 }
